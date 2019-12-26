7f8a078757c9ea68e6d5571439d161d00b32beda
titombo <titombo@gmail.com>
2019-12-26 19:50:59 +0100

    3 files modified, 1 added

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
+  template: `<h1>Hello {{name}}!</h1> <h1> {{this.inputString}} </h1>`,
   styles: [`h1 { font-family: Lato; }`]
 })
-export class HelloComponent  {
+export class HelloComponent implements OnInit {
   @Input() name: string;
+
+  inputString:any;
+
+  constructor(private httpClient: HttpClient){}
+
+  ngOnInit(){
+    this.httpClient.get("/assets/input.json").subscribe(data =>{
+      console.log(data);
+      this.inputString = JSON.stringify(data);
+    })
+  }
 }
